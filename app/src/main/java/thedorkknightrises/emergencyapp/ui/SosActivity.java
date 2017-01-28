a496106c3fa77fe4d82eeb585d976868f55b53cd
TheDorkKnightRises <samriddhabasu@gmail.com>
2017-01-29 00:32:22 +0530

    toolbar

--- app/src/main/java/thedorkknightrises/emergencyapp/ui/SosActivity.java
+++ app/src/main/java/thedorkknightrises/emergencyapp/ui/SosActivity.java
@@ -3,6 +3,8 @@ package thedorkknightrises.emergencyapp.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -17,6 +19,12 @@ public class SosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sos);
+
+        Toolbar toolbar =(Toolbar)findViewById(R.id.activity_sos_toolbar);
+        setSupportActionBar(toolbar);
+        setTitle("");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.svg_clear_white_36px);
 
         button=(Button)findViewById(R.id.button2);
 
@@ -34,12 +42,24 @@ public class SosActivity extends AppCompatActivity {
         {
             @Override
             public void onClick(View view) {
-                Intent myIntent2 = new Intent(SosActivity.this,SignInActivity.class);
-                startActivity(myIntent2);
+
             }
 
 
         });
 
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case android.R.id.home:
+                Intent myIntent2 = new Intent(SosActivity.this,SignInActivity.class);
+                startActivity(myIntent2);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
