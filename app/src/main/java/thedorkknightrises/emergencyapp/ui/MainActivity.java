6c8a87dd751432eb9dae95a9c86fa24e30ff3eb2
TejasBhitle <bhitletejas@gmail.com>
2017-01-28 18:34:11 +0530

    Sign in Activity added

--- app/src/main/java/thedorkknightrises/emergencyapp/ui/MainActivity.java
+++ app/src/main/java/thedorkknightrises/emergencyapp/ui/MainActivity.java
@@ -1,15 +1,38 @@
 package thedorkknightrises.emergencyapp.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import thedorkknightrises.emergencyapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    FirebaseAuth auth;
+    final String LOG ="MAINACTIVITY";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
+
+    private void checkUserSignedIn(){
+        //to check if the user is signed in
+        auth = FirebaseAuth.getInstance();
+        if(auth.getCurrentUser() != null){
+
+        }
+        else{
+            Log.e(LOG,"is NULL");
+            Intent intent = new Intent(MainActivity.this,SignInActivity.class);
+            //startActivity(intent);
+        }
+    }
+
 }
